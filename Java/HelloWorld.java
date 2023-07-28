75dc2e36ffcad70bddd814c1ced245aedd972240
Milind <m.milind15@gmail.com>
2023-07-28 14:07:45 +0530

    update

---
+++ Java/HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class HelloWorld {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a");
+    int a = sc.nextInt();
+    System.out.println("why u enter: " + a); 
+  }
+}
