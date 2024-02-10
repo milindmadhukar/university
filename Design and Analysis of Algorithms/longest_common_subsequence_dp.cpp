9c2816c164900b29d0a089ca8acb708f24bbbfe3
Milind <m.milind15@gmail.com>
2024-02-10 19:14:38 +0530

    printing lcs

--- Design and Analysis of Algorithms/longest_common_subsequence_dp.cpp
+++ Design and Analysis of Algorithms/longest_common_subsequence_dp.cpp
@@ -4,42 +4,50 @@ int dp[100][100];
 char dir[100][100];
 
 int lcs(const std::string &X, const std::string &Y, int m, int n) {
-
-  // Initializing a matrix of size
-  // (m+1)*(n+1)
-
-  // Following steps build L[m+1][n+1]
-  // in bottom up fashion. Note that
-  // L[i][j] contains length of LCS of
-  // X[0..i-1] and Y[0..j-1]
   for (int i = 0; i <= m; i++) {
     for (int j = 0; j <= n; j++) {
       if (i == 0 || j == 0) {
         dp[i][j] = 0;
-      }
-
-      else if (X[i - 1] == Y[j - 1])
+      } else if (X[i - 1] == Y[j - 1]) {
         dp[i][j] = dp[i - 1][j - 1] + 1;
-
-      else
-        dp[i][j] = fmax(dp[i - 1][j], dp[i][j - 1]);
+        dir[i][j] = 'd';
+      } else {
+        if (dp[i - 1][j] > dp[i][j - 1]) {
+          dp[i][j] = dp[i - 1][j];
+          dir[i][j] = 'u';
+        } else {
+          dp[i][j] = dp[i][j - 1];
+          dir[i][j] = 'l';
+        }
+      }
     }
   }
-
-  // L[m][n] contains length of LCS
-  // for X[0..n-1] and Y[0..m-1]
   return dp[m][n];
+}
+
+void find_sequence(const std::string &X, int m, int n) {
+  if (dp[m][n] == dp[m-1][n]) { // up
+    find_sequence(X, m-1, n);
+  } else if (dp[m][n] == dp[m][n-1]) { // left
+    find_sequence(X, m, n-1);
+  } else if (dp[m][n] == dp[m-1][n-1] + 1) { // diagonal
+    find_sequence(X, m-1, n-1);
+    std::cout << X[m-1];
+  }
 }
 
 // Driver code
 int main() {
-  std::string S1 = "longest";
-  std::string S2 = "stone";
-  int m = S1.size();
-  int n = S2.size();
+  std::string X = "longest";
+  std::string Y = "stone";
+  int m = X.size();
+  int n = Y.size();
 
   // Function call
-  std::cout << "Length of LCS is " << lcs(S1, S2, m, n) << std::endl;
+  std::cout << "Length of LCS is " << lcs(X, Y, m, n) << std::endl;
+  std::cout << "LCS is: ";
+  find_sequence(X, m, n);
+  std::cout << std::endl;
 
   return 0;
 }
