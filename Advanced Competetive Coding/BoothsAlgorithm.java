93013f8d6fc8457ed1be46d24e2c78209e505df5
Milind <m.milind15@gmail.com>
2024-10-19 00:48:08 +0530

    booths algorithm

---
+++ Advanced Competetive Coding/BoothsAlgorithm.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class BoothsAlgorithm {
+  public static int multiply(int n1, int n2) {
+    int r = n2;
+    int A = n1;
+    int P = 0;
+    int count = Integer.SIZE;
+    while (count > 0) {
+      if ((r & 1) == 1) {
+        P += A;
+      }
+      A <<= 1;
+      count--;
+      r >>= 1;
+    }
+    return P;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter two integers: ");
+    int n1 = sc.nextInt();
+    int n2 = sc.nextInt();
+    sc.close();
+    int result = multiply(n1, n2);
+    System.out.println("\n\nResult : " + n1 + " * " + n2 + " = " + result);
+  }
+}
